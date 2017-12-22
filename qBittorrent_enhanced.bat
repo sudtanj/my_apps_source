f39a6060c2c494365259907a4cce89ae059540f4
Sudono Tanjung <sudtanj@users.noreply.github.com>
2017-12-22 16:32:34 +0700

    Add files via upload

---
+++ qBittorrent_enhanced.bat
@@ -0,0 +1,14 @@
+@echo off
+D:
+cd D:\Apps\PortableAppz\PeerBlockPortable
+start PeerBlockPortable.exe
+cd D:\Apps\PortableAppz\qBittorrentPortable
+START /B /wait qBittorrentPortable.exe
+:REKILL
+taskkill /IM peerblock.exe
+taskkill /IM PeerBlockPortable.exe
+tasklist /FI "IMAGENAME eq peerblock.exe" 2>NUL | find /I /N "peerblock.exe">NUL
+if "%ERRORLEVEL%"=="0" ( GOTO :REKILL )
+tasklist /FI "IMAGENAME eq PeerBlockPortable.exe" 2>NUL | find /I /N "PeerBlockPortable.exe">NUL
+if "%ERRORLEVEL%"=="0" ( GOTO :REKILL )
+pause
